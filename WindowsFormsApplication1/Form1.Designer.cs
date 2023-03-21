7a2432fa16533774f01f9f4610229224aec1cd4a
bah8k <63976738+bah8k@users.noreply.github.com>
2023-03-21 15:29:32 +0300

    first commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.back = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Location = new System.Drawing.Point(407, 279);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.Location = new System.Drawing.Point(456, 280);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox3.Location = new System.Drawing.Point(533, 280);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox4.Location = new System.Drawing.Point(581, 279);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox4.TabIndex = 2;
+            this.pictureBox4.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 50F);
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(485, 278);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 101);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 50F);
+            this.label2.ForeColor = System.Drawing.Color.Lime;
+            this.label2.Location = new System.Drawing.Point(609, 278);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 101);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ":";
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox5.Location = new System.Drawing.Point(653, 280);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox6.Location = new System.Drawing.Point(704, 280);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(54, 100);
+            this.pictureBox6.TabIndex = 6;
+            this.pictureBox6.TabStop = false;
+            // 
+            // back
+            // 
+            this.back.BackColor = System.Drawing.Color.Transparent;
+            this.back.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.back.Location = new System.Drawing.Point(12, 12);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(1367, 929);
+            this.back.TabIndex = 8;
+            this.back.TabStop = false;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1112, 644);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.back);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.back)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.PictureBox back;
+
+
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,260 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void drawLine(Point p1, Point p2, Bitmap bitmap, bool colorIsGreen = true)
+        {
+            int x = p1.X;
+            int y = p1.Y;
+            int deltaX = Math.Abs(p2.X - p1.X);
+            int deltaY = Math.Abs(p2.Y - p1.Y);
+            int signX = Math.Sign(p2.X - p1.X);
+            int signY = Math.Sign(p2.Y - p1.Y);
+            int interchange = 0, e, a, b, t;
+
+            if (deltaY > deltaX)
+            {
+                t = deltaX;
+                deltaX = deltaY;
+                deltaY = t;
+                interchange = 1;
+            }
+
+
+            e = 2 * deltaY - deltaX;
+            a = 2 * deltaY;
+            b = 2 * deltaY - 2 * deltaX;
+            try
+            {
+                bitmap.SetPixel((int)x, (int)y, colorIsGreen ? Color.Green : Color.White);
+            }
+            catch (Exception exx) { 
+            }
+
+            for (int i = 1; i <= deltaX; i++)
+            {
+                if (e < 0)
+                {
+                    if (interchange == 1)
+                    {
+                        y = y + signY;
+                    }
+                    else
+                    {
+                        x = x + signX;
+                    }
+                    e = e + a;
+                }
+                else
+                {
+                    y = y + signY;
+                    x = x + signX;
+                    e = e + b;
+                }
+                    
+                    try
+                    {
+                        bitmap.SetPixel((int)x, (int)y, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x + 1, (int)y, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x, (int)y + 1, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x + 1, (int)y + 1, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x - 1, (int)y, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x, (int)y - 1, colorIsGreen ? Color.Green : Color.White);
+                        bitmap.SetPixel((int)x - 1, (int)y - 1, colorIsGreen ? Color.Green : Color.White);
+                    }
+                    catch (Exception ex)
+                    {
+                        //  Block of code to handle errors
+                    }
+
+            }
+        }
+        private void arc(Point point, double r, int sa, int ea, Bitmap bitmap){
+            double x, y;
+            double theta, dtheta = 1 / r, gama;
+            if (sa > ea)
+                ea += 360;
+            theta = sa * 3.14 / 180;
+            gama = ea * 3.14 / 180;
+            while (theta < gama)
+            {
+                x = point.X + r * Math.Cos(theta);
+                y = point.Y - r * Math.Sin(theta);
+                bitmap.SetPixel((int)x, (int)y, Color.White);
+                bitmap.SetPixel((int)x + 1, (int)y, Color.White);
+                bitmap.SetPixel((int)x, (int)y + 1, Color.White);
+                bitmap.SetPixel((int)x + 1, (int)y + 1, Color.White);
+                bitmap.SetPixel((int)x - 1, (int)y, Color.White);
+                bitmap.SetPixel((int)x, (int)y - 1, Color.White);
+                bitmap.SetPixel((int)x - 1, (int)y - 1,Color.White);
+                theta = theta + dtheta;
+            }
+
+
+        }
+        void drawNum(int num,Bitmap bitmap){
+
+            //خط اليسرة
+            if (num == 0 || num == 4 || num == 5 || num == 6 || num == 7 || num == 8 || num == 9) drawLine(new Point(10, 20), new Point(10, 40), bitmap);
+            if (num == 0 || num == 2 || num == 6 || num == 8) drawLine(new Point(10, 45), new Point(10, 65), bitmap);
+
+            //خط الفوك
+            if (num == 0 || num == 2 || num == 3  || num == 5 || num == 6 || num == 7 || num == 8 || num == 9) drawLine(new Point(12, 18), new Point(28, 18), bitmap);
+
+            //خط اليمنة
+            if (num == 0 || num == 1 || num == 2 || num == 3 || num == 4 || num == 7 || num == 8 || num == 9) drawLine(new Point(30, 20), new Point(30, 40), bitmap);
+            if (num == 0 || num == 1 || num == 3 || num == 4 || num == 5 || num == 6 || num == 7 || num == 8 || num == 9) drawLine(new Point(30, 45), new Point(30, 65), bitmap);
+
+            //خط النص
+            if (num == 2 || num == 3 || num == 4 || num == 5 || num == 6 || num == 8 || num == 9) drawLine(new Point(12, 44), new Point(28, 44), bitmap);
+
+            //خط الجوة
+            if (num == 0 || num == 2 || num == 3 || num == 5 || num == 6 || num == 8 || num == 9) drawLine(new Point(12, 68), new Point(28, 68), bitmap);
+
+        }
+        void drawTime(int time, Bitmap bitmap1, Bitmap bitmap2, Bitmap bitmap3, Bitmap bitmap4) {
+
+            if (time < 1000) drawNum(0, bitmap1);
+            else drawNum(time/1000, bitmap1);
+
+
+            int cur = time/1000;
+            time = time - cur * 1000;
+
+            if (time < 100) drawNum(0, bitmap2);
+            else drawNum(time / 100, bitmap2);
+
+
+            cur = time / 100;
+            time = time - cur * 100;
+
+            if (time < 10) drawNum(0, bitmap3);
+            else drawNum(time / 10, bitmap3);
+
+            cur = time / 10;
+            time = time - cur * 10;
+
+            if (time < 1) drawNum(0, bitmap4);
+            else drawNum(time / 1, bitmap4);
+
+
+        }
+        void drawSwa(Point position, Bitmap bitmap7) {
+
+            //S
+            arc(new Point(100 + position.X, 100 + position.Y), 30, 0, 260, bitmap7);
+            arc(new Point(84 + position.X, 159 + position.Y), 30, 230, 70, bitmap7);
+            //W
+            drawLine(new Point(140 + position.X, 70 + position.Y), new Point(170 + position.X, 200 + position.Y), bitmap7, colorIsGreen: false);
+            drawLine(new Point(200 + position.X, 70 + position.Y), new Point(170 + position.X, 200 + position.Y), bitmap7, colorIsGreen: false);
+            drawLine(new Point(200 + position.X, 70 + position.Y), new Point(220 + position.X, 200 + position.Y), bitmap7, colorIsGreen: false);
+            drawLine(new Point(270 + position.X, 70 + position.Y), new Point(220 + position.X, 200 + position.Y), bitmap7, colorIsGreen: false);
+
+            //A
+            int x = 130;
+            drawLine(new Point(120 + x + position.X, 188 + position.Y), new Point(175 + x + position.X, 70 + position.Y), bitmap7, colorIsGreen: false);
+            drawLine(new Point(175 + x + position.X, 70 + position.Y), new Point(220 + x + position.X, 188 + position.Y), bitmap7, colorIsGreen: false);
+            drawLine(new Point(150 + x + position.X, 125 + position.Y), new Point(195 + x + position.X, 125 + position.Y), bitmap7, colorIsGreen: false);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        
+
+        }
+
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        
+        Bitmap bitmap1 = new Bitmap(500, 500);
+        Bitmap bitmap2 = new Bitmap(500, 500);
+        Bitmap bitmap3 = new Bitmap(500, 500);
+        Bitmap bitmap4 = new Bitmap(500, 500);
+        Bitmap bitmap5 = new Bitmap(500, 500);
+        Bitmap bitmap6 = new Bitmap(500, 500);
+        Bitmap bitmap7 = new Bitmap(500, 500);
+
+        int xpub = 0;
+        bool flag = false;
+        int latnzy = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            try
+            {
+                bitmap1 = new Bitmap(500, 500);
+                bitmap2 = new Bitmap(500, 500);
+                bitmap3 = new Bitmap(500, 500);
+                bitmap4 = new Bitmap(500, 500);
+                bitmap5 = new Bitmap(500, 500);
+                bitmap6 = new Bitmap(500, 500);
+                bitmap7 = new Bitmap(this.Height, this.Width);
+
+            }catch(Exception eee){}
+            DateTime localDate = DateTime.Now;
+            String time = "";
+            time += localDate.Hour.ToString();
+            if (localDate.Minute < 10) time += 0;
+            time += localDate.Minute.ToString();
+            //MessageBox.Show(time);
+            drawTime(Convert.ToInt16(time), bitmap1, bitmap2, bitmap3, bitmap4);
+           // drawSwa(new Point(localDate.Second * localDate.Second, localDate.Second * localDate.Second), bitmap7);
+            drawSwa(new Point(0, xpub), bitmap7);
+            drawSwa(new Point(xpub, 0), bitmap7);
+            latnzy++;
+            if (flag && latnzy > 50) xpub-=10;
+            else if(latnzy > 50) xpub+=10;
+            if (xpub == 200) flag = true;
+            else if (xpub == 0) {
+                flag = false;
+            }
+
+
+            if (localDate.Second > 9) drawNum(Convert.ToInt16((localDate.Second / 10)), bitmap5);
+            else drawNum(0, bitmap5);
+
+            drawNum(Convert.ToInt16(localDate.Second - (localDate.Second / 10)*10), bitmap6);
+
+            pictureBox1.Image = bitmap1;
+            pictureBox2.Image = bitmap2;
+            pictureBox3.Image = bitmap3;
+            pictureBox4.Image = bitmap4;
+            pictureBox5.Image = bitmap5;
+            pictureBox6.Image = bitmap6;
+            back.Image = bitmap7;
+
+
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    
+    }
+}
